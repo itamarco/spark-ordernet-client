76e721b7a4cfc6c5dd05153eea8b015f2a31cf36
Itamar <itc404@gmail.com>
2020-11-28 20:24:32 +0200

    response keys convertor

---
+++ src/response-keys-converter/data-keys-converter.ts
@@ -0,0 +1,32 @@
+import {KeyValue} from "../models/key-value.type";
+import {AuthResult, RMType, StructAccountTransaction} from "./response-maps";
+
+const RESPONSE_TYPES = {
+    AuthResult: AuthResult,
+    StructAccountTransaction: StructAccountTransaction,
+    RMType: RMType,
+}
+
+export function convertKeys(data: KeyValue): KeyValue {
+    const responseType = data._t as keyof typeof RESPONSE_TYPES;
+    const keysMap = RESPONSE_TYPES[responseType] || {};
+
+    const res = Object.keys(data).reduce((acc, curKey) => {
+        const newKey = keysMap[curKey] as string || curKey;
+        acc[newKey] = data[curKey]
+        return acc;
+    }, {} as KeyValue);
+
+    return res;
+}
+// export function dataConvertorFactory(dataType: string) {
+//     const keysMap = DATA_TYPES[dataType] as KeyValue;
+//     return (data: KeyValue) => {
+//         const res = Object.keys(data).reduce((acc, curKey) => {
+//             const newKey = keysMap[curKey] || curKey;
+//             acc[newKey] = data[curKey]
+//             return acc;
+//         }, {} as KeyValue);
+//         return res;
+//     }
+// }
