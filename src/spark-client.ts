f3677eb2b51668f92ef1ff7ae1aa5c1f23ba9ad1
Itamar <itc404@gmail.com>
2021-02-11 10:20:32 +0200

    debug

--- src/spark-client.ts
+++ src/spark-client.ts
@@ -3,18 +3,19 @@ import Response from "./response";
 import {ClientConfig} from "./models/client-config.interface";
 import {convertKeys} from "./response-keys-converter/data-keys-converter";
 import {KeyValue} from "./models/key-value.type";
+import curlirize from 'axios-curlirize';
 
 const PATHS = {
-    AUTH_API: "/api/Auth/Authenticate",
-    HOLDINGS_API: "/api/Account/GetHoldings",
+    AUTH_API: "api/Auth/Authenticate",
+    HOLDINGS_API: "api/Account/GetHoldings",
     HOLDINGS_SUMMARY_API: "/api/Account/GetHoldingsSummery",
-    STATIC_DATA: "/api/DataProvider/GetStaticData",
-    TRANSACTIONS: "/api/Account/GetAccountTransactions",
-    SECURITIES: "/api/Account/GetAccountSecurities",
+    STATIC_DATA: "api/DataProvider/GetStaticData",
+    TRANSACTIONS: "api/Account/GetAccountTransactions",
+    SECURITIES: "api/Account/GetAccountSecurities",
 }
 
 const USER_AGENT =
-    "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36 OPR/65.0.3467.48";
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36 OPR/65.0.3467.48";
 
 export default class SparkClient {
     private httpClient: AxiosInstance;
@@ -30,14 +31,14 @@ export default class SparkClient {
                 'Accept': 'application/json, text/plain, */*',
                 'Content-Type': 'application/json;charset=UTF-8',
                 'Referer': config.sparkHost,
-                'User-Agent': USER_AGENT,
+                'User-agent': USER_AGENT,
                 'Sec-Fetch-Mode': 'cors',
             },
-
             // validateStatus: (status: number ) => true,
         });
-
         this.username = config.userId;
+
+        curlirize(this.httpClient);
         this.password = config.password;
         this.accountKey = config.accountKey || '';
         this.logger = config.logger || console;
@@ -50,6 +51,11 @@ export default class SparkClient {
                 password: this.password,
             },
         )
+            .then( res => {
+                const cookie = res.headers['set-cookie'].join(';')
+                this.httpClient.defaults.headers['Cookie'] = cookie;
+                return res;
+            })
             .then(res => res.data)
             .then( convertKeys )
             .then( data => {
@@ -133,7 +139,7 @@ export default class SparkClient {
     }
 
     setClientBearerToken(bearerToken: string): void {
-        this.httpClient.defaults.headers.common['Authorization'] = `Bearer ${bearerToken}`;
+        this.httpClient.defaults.headers['Authorization'] = `Bearer ${bearerToken}`;
     }
 
     setSparkAccountKey(accountKey: string): void {
